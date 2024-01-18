21511cb9b1b0844958965f5465cfbb951df77805
Глеб <147049192+gleb734@users.noreply.github.com>
2024-01-18 14:05:17 +0300

    Create referee.py

---
+++ verification/referee.py
@@ -0,0 +1,19 @@
+from checkio.signals import ON_CONNECT
+from checkio import api
+from checkio.referees.io_template import CheckiOReferee
+# from checkio.referees.checkers import to_list
+
+from tests import TESTS
+
+api.add_listener(
+    ON_CONNECT,
+    CheckiOReferee(
+        tests=TESTS,
+        # checker=to_list,
+        function_name={
+            "python": "riichi_mahjong_yaku"
+        },
+        cover_code={
+            'python-3': {}
+        }
+    ).on_ready)
